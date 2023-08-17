93beb5b8d777e201618bcf898993d00227663d7a
izabelladobielewska <137038301+izabelladobielewska@users.noreply.github.com>
2023-08-17 12:00:34 +0200

    Update components/Header/index.js
    Co-authored-by: Merle Makoben <merlemakoben@gmail.com>

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,12 +5,12 @@ export default function Header() {
   return (
     <header>
       <h1>Endstagram</h1>
-      <Button>
+
         <Link href="/">Home</Link>
-      </Button>
-      <Button>
+  
+     
         <Link href="/preferences-form">Preferences</Link>
-      </Button>
+
     </header>
   );
 }
