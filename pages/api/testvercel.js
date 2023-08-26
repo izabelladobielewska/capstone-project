521e1c3a5098304734f2b4ebc1e1c16714988fe9
izabella dobielewska <izadobielewska@yahoo.co.uk>
2023-08-26 17:02:29 +0200

    update test route, status code

--- pages/api/testvercel.js
+++ pages/api/testvercel.js
@@ -1,5 +1,5 @@
 import clientPromise from "@/utils/connectMongo";
 
 export default async function handler(req, res) {
-  res.status(204).json({ foo: "bar" });
+  res.status(201).json({ foo: "bar" });
 }
