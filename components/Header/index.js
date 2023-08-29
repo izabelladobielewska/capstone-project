e98d25b6c7d454d5257f49da92dca624b5a5344f
izabella dobielewska <izadobielewska@yahoo.co.uk>
2023-08-29 13:37:25 +0200

    add some comments to the redundant code

--- components/Header/index.js
+++ components/Header/index.js
@@ -20,7 +20,8 @@ export default function Header() {
       <BrandLink href="/" pathname={pathName}>
         <h1>Endstagram</h1>
       </BrandLink>
-      {/* <LikesLink href="/my-likes" pathname={pathName}>
+      {/*for later
+       <LikesLink href="/my-likes" pathname={pathName}>
         <LikesIcon
           alt="link to my likes page"
           color="black"
@@ -74,6 +75,7 @@ const AddLink = styled(Link)`
   padding: 0;
   text-decoration: none;
 `;
+// for later
 // const LikesLink = styled(Link)`
 //   grid-column: 7;
 //   place-self: center;
